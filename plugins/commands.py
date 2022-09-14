c5613333451562dc8c7cd854b1420ceac1034164
Brokenboy204 <111448245+Brokenboy204@users.noreply.github.com>
2022-09-14 11:51:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -235,7 +235,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/OpusTechz') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://t.me/TG_Updates_204') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
